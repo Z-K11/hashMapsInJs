fa0ac36d96fdb475423000ec2d7a237f5b910250
theZK11 <zulqarnainbazidkhail@gmail.com>
2026-08-10 07:41:43 +0500

    add methods has(),remove(),length() and clear() has takes a key as an arguement returns true if key exists remove takes key as an arguement and removes it's value length returns the number of keys assigned clear clears the whole hashtable

--- src/hashMap.js
+++ src/hashMap.js
@@ -6,10 +6,10 @@ class hashMap {
   constructor() {
     this.#capacity = 16;
     this.#loadFactor = 0.75;
-    this.#table = new Array(this.#capacity).fill(0);
+    this.#table = new Array(this.#capacity).fill(null);
   }
 
-  // Has function to create array indeces using keys
+  // Has function to create array indices using keys
   hash(key) {
     let hashCode = 0;
     const primeNumber = 31;
@@ -28,5 +28,26 @@ class hashMap {
   // get function to retreive a value from the table using it's key
   get(key) {
     return this.#table[this.hash(key)];
+  }
+
+  has(key) {
+    return this.#table[key] !== null;
+  }
+  remove(key) {
+    if (!(this.#table[key] !== null)) return false;
+    else {
+      this.#table[key] = null;
+      return true;
+    }
+  }
+  length() {
+    let count = 0;
+    for (let i = 0; i < this.#table.length; i++) {
+      if (this.#table[i] !== null) count++;
+    }
+    return count;
+  }
+  clear() {
+    this.#table.fill(null);
   }
 }
